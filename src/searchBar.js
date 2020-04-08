41ca2533a3a01bb02c83505c344dc448bcafe83b
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-04-08 14:01:45 -0700

    Added search bar as separate .js file

---
+++ src/searchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+function searchBar() {
+  var styles;
+
+  function headerStyle() {
+    styles = {
+      color: 'red',
+    }
+  };
+
+  headerStyle();
+
+  return(
+    <center>
+      <h2 style={styles}>YouTube Clone</h2>
+      <input type="text"></input>
+      <button>Search</button>
+    </center>
+  );
+}
+
+
+export default searchBar;
