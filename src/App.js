0f9743085cc1845438879adbbeeff66ab5d11525
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-04-09 06:24:46 -0700

    Changed some formatting and added youtube api parameters. Still not quite right

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './searchBar';
 import SearchResult from './SearchResult';
+import axios from 'axios';
 
 
 class App extends Component {
@@ -14,22 +15,22 @@ class App extends Component {
   }
 
   componentDidMount(){
-    axios.get('')
+    axios.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=google')
     .then(function(response){
       this.setState({
         videoData: response, //Might want to get more specific? Maybe at index or certain key. Get only videos here I think. Or leave so it can be passed to any component
       })
     })
+  }
 
-    loadTopTen = () => {
-      var topTenVideos = [];
-      for (i=0; i<10; i++){
-        topTenVideos.push(videoData[i]); //This is going to cause issues because the response is probably only an array of 3
-      }
-      this.setState({
-        topTen: topTenVideos,
-      })
+  loadTopTen = () => {
+    var topTenVideos = [];
+    for (var i=0; i<10; i++){
+      topTenVideos.push(videoData[i]); //This is going to cause issues because the response is probably only an array of 3, and it's not what I want anyway until I get more specific
     }
+    this.setState({
+      topTen: topTenVideos,
+    })
   }
 
   render() {
